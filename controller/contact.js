279effeabc240aee77f0838fb1675a023f9a428b
Priyanshu Maddeshiya <maddeshiyapriyanshu2@gmail.com>
2026-06-20 13:24:14 +0530

    nodemailor

--- controller/contact.js
+++ controller/contact.js
@@ -7,28 +7,31 @@ export const sendContactEmail = async (req, res) => {
     return res.status(400).json({ error: "All fields are required." });
 
   try {
-   const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true sirf port 465 ke liye
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true sirf port 465 ke liye
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
+    });
 
-await transporter.sendMail({
-  from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
-  to: process.env.EMAIL_USER,
-  replyTo: email,
-  subject: `New Message from ${name}`,
-  html: `
+    await transporter.sendMail({
+      from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_USER,
+      replyTo: email,
+      subject: `New Message from ${name}`,
+      html: `
     <h3>New Contact Form Submission</h3>
     <p><b>Name:</b> ${name}</p>
     <p><b>Email:</b> ${email}</p>
     <p><b>Message:</b><br/>${message}</p>
   `,
-});
+    });
 
     res.status(200).json({ message: "Email sent successfully." });
   } catch (error) {
