5f750c09da0927b3a1c0344a838ad12e6607b5b8
Priyanshu Maddeshiya <maddeshiyapriyanshu2@gmail.com>
2026-07-09 14:16:27 +0530

    nodemialor

--- controller/contact.js
+++ controller/contact.js
@@ -8,16 +8,17 @@ export const sendContactEmail = async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-
-      connectionTimeout: 10000, // 10 seconds to establish connection
-      greetingTimeout: 10000,   // 10 seconds to receive SMTP greeting
-      socketTimeout: 10000,     // 10 seconds of inactivity
-
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     // verify connection before sending
